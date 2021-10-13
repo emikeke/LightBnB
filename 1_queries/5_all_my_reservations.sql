79bdf8ad1c88da281464b96b7d5fc8188400fce9
Emily K <emily.ke@hotmail.com>
2021-10-13 19:18:11 -0400

    made queries 1

---
+++ 1_queries/5_all_my_reservations.sql
@@ -0,0 +1,9 @@
+SELECT properties.id AS id, title, cost_per_night, start_date, avg(property_reviews.rating) AS average_rating
+FROM properties
+JOIN reservations ON properties.id = reservations.property_id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.end_date < now()::date 
+AND reservations.guest_id = 1
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT 10;
