4100cdfed82cec539ff1b02949f7faa1e474cf39
Emily K <emily.ke@hotmail.com>
2021-10-14 18:56:39 -0400

    reservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -90,7 +90,22 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
  const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const getAllReservationsQuery = `
+  SELECT properties.id AS id, title, cost_per_night, start_date, avg(property_reviews.rating) AS average_rating
+  FROM properties
+  JOIN reservations ON properties.id = reservations.property_id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.end_date < now()::date 
+  AND reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`
+
+  return db.query(getAllReservationsQuery, [guest_id, limit])
+  .then(res => {
+    return res.rows;
+  })
+  .catch(error => console.log(error));
 }
 exports.getAllReservations = getAllReservations;
 
